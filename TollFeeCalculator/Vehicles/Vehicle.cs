061ec30b911df73235fa0751aed29d069458bf18
Taner Yavuz <taner.yavuz@outlook.com>
2024-09-03 10:05:13 +0200

    Change TollFree property to be set once

--- TollFeeCalculator/Vehicles/Vehicle.cs
+++ TollFeeCalculator/Vehicles/Vehicle.cs
@@ -3,19 +3,21 @@
 /// <summary>
 /// The implementation of a vehicle
 /// </summary>
-public class Vehicle(VehicleType typeOfVehicle) : IVehicle
+public class Vehicle : IVehicle
 {
-    public VehicleType TypeOfVehicle { get; } = typeOfVehicle;
+   public VehicleType TypeOfVehicle { get; }
 
-    public bool TollFree 
-    {
-        get
-        {
-            return TypeOfVehicle switch
-            {
-                VehicleType.Motorbike or VehicleType.Tractor or VehicleType.Emergency or VehicleType.Diplomat or VehicleType.Foreign or VehicleType.Military => true,
-                _ => false
-            };
-        }
-    }         
+   public bool TollFree { get; }
+
+   
+   public Vehicle(VehicleType typeOfVehicle)
+   {
+      // Set properties once to avoid calculating value each time property is accessed 
+      TypeOfVehicle = typeOfVehicle;
+      TollFree = TypeOfVehicle switch
+      {
+         VehicleType.Motorbike or VehicleType.Tractor or VehicleType.Emergency or VehicleType.Diplomat or VehicleType.Foreign or VehicleType.Military => true,
+         _ => false
+      };
+   }
 }
